b4420da8375759d324aea3e848de6b8b9ab9330d
Abhay <abhay199658@gmail.com>
2020-01-12 14:19:11 +0530

    [upd] languages are welcome

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import { I18n } from 'aws-amplify';
 
 @Component({
@@ -9,10 +9,26 @@ import { I18n } from 'aws-amplify';
 })
 export class WelcomeComponent implements OnInit {
   welcome = I18n.get('Welcome');
+  lang = 'en';
 
-  constructor() { }
+  constructor(private cdr: ChangeDetectorRef) { }
 
   ngOnInit() {
+    this.setLanguage(this.lang);
   }
 
+  toggleLanguage() {
+    if (this.lang === 'en') {
+      this.setLanguage('esp');
+    } else {
+      this.setLanguage('en');
+    }
+  }
+
+  setLanguage(lang: string) {
+    this.lang = lang;
+    I18n.setLanguage(lang);
+    this.welcome = I18n.get('Welcome');
+    this.cdr.detectChanges();
+  }
 }
