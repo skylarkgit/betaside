2ff1fcbc9dfbb12607750e907d9c3216c85c290c
Abhay <abhay199658@gmail.com>
2020-01-16 00:07:13 +0530

    [fix] env prefix

--- custom-webpack-config.js
+++ custom-webpack-config.js
@@ -4,11 +4,11 @@ module.exports = {
   plugins: [
     new webpack.DefinePlugin({
       $ENV: {
-        AWS_CLIENT_ID: JSON.stringify(process.env.AWS_CLIENT_ID),
-        AWS_CLIENT_SECRET: JSON.stringify(process.env.AWS_CLIENT_SECRET),
-        SIGNIN_REDIRECT_URI: JSON.stringify(process.env.SIGNIN_REDIRECT_URI),
-        SIGNOUT_REDIRECT_URI: JSON.stringify(process.env.SIGNOUT_REDIRECT_URI),
-        SIGNIN_URI: JSON.stringify(process.env.SIGNIN_URI),
+        AWS_CLIENT_ID: JSON.stringify(process.env.BTSD_AWS_CLIENT_ID),
+        AWS_CLIENT_SECRET: JSON.stringify(process.env.BTSD_AWS_CLIENT_SECRET),
+        SIGNIN_REDIRECT_URI: JSON.stringify(process.env.BTSD_SIGNIN_REDIRECT_URI),
+        SIGNOUT_REDIRECT_URI: JSON.stringify(process.env.BTSD_SIGNOUT_REDIRECT_URI),
+        SIGNIN_URI: JSON.stringify(process.env.BTSD_SIGNIN_URI),
       }
     })
   ]
